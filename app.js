01b355355d077b965c6a004e6901f365a2a5c79a
stefbar <stef.barucq@gmail.com>
2024-05-21 09:44:05 +0200

    new branch swagger

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
 const express = require('express')
-// const router = require('express').Router()
-// const swaggerJsdoc = require('swagger-jsdoc')
-// const swaggerUi = require('swagger-ui-express')
-// const swaggerDocument = require('./swagger.json')
-
 const sequelize = require('./db')
 const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT
 const jokesRoutes = require('./routes/jokesRoutes')
-
-// const swaggerOptions = {
-//     swaggerDefinition: {
-//         info: {
-//             title: 'Carambar Jokes API',
-//             version: '1.0.0'
-//         }
-//     },
-//     apis: ['./app.js']
-// }
-// const swaggerDocs = swaggerJsdoc(swaggerOptions)
-
-// app.use('/api-docs', swaggerUi.serve)
-// router.get('/api-docs', swaggerUi.setup(swaggerDocs))
 
 app.use(cors())
 // const corsOptions = {
