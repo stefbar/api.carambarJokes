dbb3694754080050a9e5866360b027250f62c9ef
stefbar <stef.barucq@gmail.com>
2024-10-04 11:35:16 +0200

    added loading animation when waiting for random joke

--- controllers/jokesControllers.js
+++ controllers/jokesControllers.js
@@ -52,6 +52,10 @@ exports.getRandomJoke = async (req, res) => {
     try {
         const randomId = await Math.floor(Math.random() * 10 + 1)
         const joke = await CarambarJokes.findByPk(randomId)
+        if(!joke) {
+            res.status(404).send(`Joke ${randomId} not found`)
+            return
+        }
         res.json(joke)
     } catch(err) {
         res.status(500).send(err)
