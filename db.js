a4d79600e9c90dd796051395a3df250dd351c86f
stef bar <stef.barucq@gmail.com>
2024-10-30 12:09:26 +0100

    try to fix db in prod

--- db.js
+++ db.js
@@ -10,6 +10,37 @@ const sequelize = new Sequelize({
       timestamps: false // don't create created_at and updated_at columns
     }
 })
+
+async function testConnection() {
+  try {
+      await sequelize.authenticate()
+      console.log('Connection has been established successfully.')
+  } catch (error) {
+      console.error('Unable to connect to the database:', error)
+  }
+}
+// testConnection()
+
+async function checkTables() {
+  const [results] = await sequelize.query("SELECT name FROM sqlite_master WHERE type='table'")
+  console.log('Tables in database:', results)
+}
+// checkTables()
+
+async function checkData() {
+  const [results] = await sequelize.query("SELECT COUNT(*) as count FROM CarambarJokes");
+  console.log('Number of jokes:', results[0].count);
+}
+// checkData()
+
+async function init() {
+  await testConnection();
+  await checkTables();
+  await checkData();
+}
+
+init().catch(err => console.error(err))
+
 const connectDb = async () => {
   try {
       await sequelize.sync()
