f642782ef0def79af65663702c218667df58bad4
stef bar <stef.barucq@gmail.com>
2025-12-06 18:22:59 +0100

    changed jokes contained in db seed

--- data/dbSeed.js
+++ data/dbSeed.js
@@ -17,47 +17,110 @@ async function seedDatabase() {
         }
     ]
 
+    // const jokes = [
+    //     {
+    //         id: 1,
+    //         joke: "Quelle est la femelle du hamster ?",
+    //         answer: "L’Amsterdam",
+    //         category_id: 2
+    //     },
+    //     {
+    //         id: 2,
+    //         joke: "Que dit un oignon quand il se cogne ?",
+    //         answer: "Aïe !",
+    //         category_id: 2
+    //     },
+    //     {
+    //         id: 3,
+    //         joke: "Quel est l'animal le plus heureux ?",
+    //         answer: "Le hibou, parce que sa femme est chouette.",
+    //         category_id: 2
+    //     },
+    //     {
+    //         id: 4,
+    //         joke: "Pourquoi le football c'est rigolo ?",
+    //         answer: "Parce que Thierry en rit",
+    //         category_id: 2
+    //     },
+    //     {
+    //         id: 5,
+    //         joke: "Quel est le sport le plus fruité ?",
+    //         answer: "La boxe, parce que tu te prends des pêches dans la poire et tu tombes dans les pommes.",
+    //         category_id: 2
+    //     },
+    //     {
+    //         id: 6,
+    //         joke: "Que se fait un Schtroumpf quand il tombe ?",
+    //         answer: "Un Bleu.",
+    //         category_id: 2
+    //     },
+    //     {
+    //         id: 7,
+    //         joke: "Quel est le comble pour un marin ?",
+    //         answer: "Avoir le nez qui coule.",
+    //         category_id: 2
+    //     },
+    //     {
+    //         id: 8,
+    //         joke: "Qu'est ce que les enfants usent le plus à l'école ?",
+    //         answer: "Le professeur",
+    //         category_id: 2
+    //     },
+    //     {
+    //         id: 9,
+    //         joke: "Quel est le sport le plus silencieux ?",
+    //         answer: "Le para-chuuuut.",
+    //         category_id: 2
+    //     },
+    //     {
+    //         id: 10,
+    //         joke: "Quel est le comble pour un joueur de bowling ?",
+    //         answer: "C’est de perdre la boule",
+    //         category_id: 2
+    //     },
+    // ]
+
     const jokes = [
         {
             id: 1,
-            joke: "Quelle est la femelle du hamster ?",
-            answer: "L’Amsterdam",
+            joke: "Qu'est-ce qui a 118 yeux et 7 dents ?",
+            answer: "Un bus rempli de personnes âgées",
             category_id: 2
         },
         {
             id: 2,
-            joke: "Que dit un oignon quand il se cogne ?",
-            answer: "Aïe !",
-            category_id: 2
+            joke: "Je ne prétends pas être Batman, je dis simplement que personne ne m'a jamais vu avec lui dans la même pièce.",
+            answer: "",
+            category_id: 1
         },
         {
             id: 3,
-            joke: "Quel est l'animal le plus heureux ?",
-            answer: "Le hibou, parce que sa femme est chouette.",
-            category_id: 2
+            joke: "Je me suis toujours demandé ce que les habitants de Versailles disent à leurs enfants quand ils laissent la lumière allumée dans toutes les pièces.",
+            answer: "",
+            category_id: 1
         },
         {
             id: 4,
-            joke: "Pourquoi le football c'est rigolo ?",
-            answer: "Parce que Thierry en rit",
-            category_id: 2
+            joke: "- Tu sais respirer par le nez ?/n - Bah oui./n - Super, tu peux fermer ta gueule alors...",
+            answer: "",
+            category_id: 1
         },
         {
             id: 5,
-            joke: "Quel est le sport le plus fruité ?",
-            answer: "La boxe, parce que tu te prends des pêches dans la poire et tu tombes dans les pommes.",
+            joke: "Pourquoi est-il déconseillé de téléphoner au volant ?",
+            answer: "Parce-qu'il ne répond jamais.",
             category_id: 2
         },
         {
             id: 6,
-            joke: "Que se fait un Schtroumpf quand il tombe ?",
-            answer: "Un Bleu.",
-            category_id: 2
+            joke: "C'est parce-que la lumière va plus vite que le son que beaucoup de gens paraissent brillants avant d'avoir l'air con...",
+            answer: "",
+            category_id: 1
         },
         {
             id: 7,
-            joke: "Quel est le comble pour un marin ?",
-            answer: "Avoir le nez qui coule.",
+            joke: "Avec quoi ramasse t'on la papaye ?",
+            answer: "Avec une foufourche.",
             category_id: 2
         },
         {
@@ -68,15 +131,15 @@ async function seedDatabase() {
         },
         {
             id: 9,
-            joke: "Quel est le sport le plus silencieux ?",
-            answer: "Le para-chuuuut.",
-            category_id: 2
+            joke: "Si t'es fière d'être Blanche-Neige, tape dans tes nains !",
+            answer: "",
+            category_id: 1
         },
         {
             id: 10,
-            joke: "Quel est le comble pour un joueur de bowling ?",
-            answer: "C’est de perdre la boule",
-            category_id: 2
+            joke: "Quand un tuyau mâle rencontre un tuyau femelle, un robi naît.",
+            answer: "",
+            category_id: 1
         },
     ]
 
